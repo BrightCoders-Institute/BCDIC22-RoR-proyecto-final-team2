40eaf2107a590e6e701181f97e75b16a33fddc8f
MarcoRodriguez23 <rodriguezlmarco23@gmail.com>
2023-02-21 17:50:59 -0600

    Add default value to permisos_vender
    I created a new migration to put a default value in permisos_vender in users
    table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_224305) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_234627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_224305) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.boolean "permisos_vender"
+    t.boolean "permisos_vender", default: true
     t.boolean "permisos_comprar"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
