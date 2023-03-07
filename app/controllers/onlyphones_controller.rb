84a1d3e5a32bda8dd8fcd63c8a53f7cc15a6e01c
AangelF <aflores31@ucol.mx>
2023-03-07 16:28:11 -0600

    add file extern to add variable api_key

--- app/controllers/onlyphones_controller.rb
+++ app/controllers/onlyphones_controller.rb
@@ -1,4 +1,6 @@
 require "rest-client"
+require "json"
+require_relative "./api_key"
 
 class OnlyphonesController < ApplicationController
 
@@ -11,7 +13,8 @@ class OnlyphonesController < ApplicationController
   private
   def request_api
     url = "https://api.device-specs.io/api/smartphones/1"
-    response = RestClient.get(url, {Authorization: 'Bearer 8a2d167edb6e4f6a52e113c893d590667a470208e78180ee442bb54741d5e1f3dbfc3ccde2649a200a298b9aa0a0b01b43456e6c41d2108d7d4957191f29c6f57fac34ea47fb6ee024ce949582618e0d9a6a2ff5240b0bfafdb5c54ed108b20b397b5b22ec5cdd2e1e4427a754761ee5848cee264d1ae8f44b04d6f1db5c61a5'})
+    puts API_KEY
+    response = RestClient.get(url, {Authorization: "Bearer #{API_KEY}"})
     JSON.parse(response.to_str)
   end
 end
