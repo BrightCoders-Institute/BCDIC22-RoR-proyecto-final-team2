aebad719040319c8cf6fb0d201e6eac3a805a2d4
M-Cordova-M <alberto971801@gmail.com>
2023-03-17 17:25:48 -0600

    using an API we added a section in the main view that displays a group of random phones

--- app/controllers/onlyphones_controller.rb
+++ app/controllers/onlyphones_controller.rb
@@ -5,8 +5,10 @@ require_relative "./api_key"
 class OnlyphonesController < ApplicationController
 
   def index
-    @phones = request_api
-    @random_phone = random
+   @phones = request_api
+   @item_ids = itemid
+   @random_phone = random
+   @best_phones = bestphones
   end
 
   def random
@@ -18,7 +20,45 @@ class OnlyphonesController < ApplicationController
     [name, price, img]
   end
 
+  def request_bestphones
+    itemid
+    responses = []
+    itemid.each do |id|
+      response = request_api(id)
+      responses << response
+      end
+    responses
+  end
+
+  def bestphones
+    phones = request_bestphones
+    array_phones = []
+    for i in 0..7
+      name = phones[i]['data']['name']
+      price = phones[i]['data']['prices'][0]['price']
+        begin
+          img = phones[i]['data']['images'][0]['url']
+          rescue NoMethodError => e
+          if e.message.include?("undefined method `[]' for nil:NilClass")
+           img = "https://picsum.photos/200"
+          else
+           img = phones[i]['data']['images'][0]['url']
+          end
+        end
+       array_phones << [name, price, img]       
+    end
+    array_phones
+  end
+
   private
+  def itemid
+    id_array = []
+      for i in 1..8
+        id_array.push(rand 1..2597)
+      end
+      id_array
+    end
+
   def request_api(id_phone="")
     url = "https://api.device-specs.io/api/smartphones/#{id_phone}?populate=*"
     response = RestClient.get(url, {Authorization: "Bearer #{API_KEY}"})
