ad5c52f0c5113d74b57a01a22cefd80a1a07b8bb
AangelF <aflores31@ucol.mx>
2023-03-22 17:00:42 -0600

    Show new phone
    Show new phone with another id without params[:id] enter manaully the id

--- app/controllers/comparador_controller.rb
+++ app/controllers/comparador_controller.rb
@@ -16,14 +16,33 @@ class ComparadorController < OnlyphonesController
     @memoria = comparador_data['data']['data']['storage']['capacity__gb']
     @camara = comparador_data['data']['data']['camera']['camera_back__mp']
     @procesador = comparador_data['data']['data']['cpu']['type']
-    @so = comparador_data['data']['data']['software']['os']
-    @name_list = nameList
-    puts nameList
+    @so = comparador_data['data']['data']['software']['os'] 
+    @compare_info =compareInfo
   end
 
-  def nameList
-    params[:id]=""
-    list_id_name = request_api
+  def compareInfo
+    id_to_compare = 2591
+    params[:id] = id_to_compare
+
+    info_to_compare = request_api
+    begin
+      img_to_compare = info_to_compare['data']['images'][0]['url']
+    rescue NoMethodError => e
+      if e.message.include?("undefined method `[]' for nil:NilClass") 
+        img_to_compare = "https://picsum.photos/200" 
+      else
+        img_to_compare = info_to_compare['data']['images'][0]['url']
+      end
+    end
+    nombre_to_compare = info_to_compare['data']['name']
+    precio_to_compare = info_to_compare['data']['prices'][0]['price']
+    pantalla_to_compare = info_to_compare['data']['data']['display']['size__inch']
+    memoria_to_compare = info_to_compare['data']['data']['storage']['capacity__gb']
+    camara_to_compare = info_to_compare['data']['data']['camera']['camera_back__mp']
+    procesador_to_compare = info_to_compare['data']['data']['cpu']['type']
+    so_to_compare = info_to_compare['data']['data']['software']['os']
+
+    [img_to_compare, nombre_to_compare, precio_to_compare, pantalla_to_compare, memoria_to_compare, camara_to_compare, procesador_to_compare, so_to_compare]
   end
 
 end
