b20f57866d92d09117bad8535ce1887c4862c6dd
AangelF <aangel.flores.r@gmail.com>
2023-04-19 16:37:56 -0600

    pop ups functions

---
+++ app/javascript/popup.js
@@ -0,0 +1,15 @@
+document.getElementById('open-popup-pago').addEventListener('click', function() {
+document.getElementById('popup-container-pago').style.display = 'flex';
+});
+
+document.getElementById('close-popup-pago').addEventListener('click', function() {
+document.getElementById('popup-container-pago').style.display = 'none';
+});
+
+document.getElementById('open-popup-limpiar').addEventListener('click', function() {
+document.getElementById('popup-container-limpiar').style.display = 'flex';
+});
+
+document.getElementById('close-popup-limpiar').addEventListener('click', function() {
+document.getElementById('popup-container-limpiar').style.display = 'none';
+});
