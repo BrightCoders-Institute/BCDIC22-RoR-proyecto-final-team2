5145bc0f48913e9e6bea32d84ab07be9d3dd20c0
M-Cordova-M <alberto971801@gmail.com>
2023-04-19 16:03:59 -0600

    Restoring master.key file for deploy

--- app/controllers/seller_controller.rb
+++ app/controllers/seller_controller.rb
@@ -1,2 +1,30 @@
 class SellerController < OnlyphonesController
+  def index
+    logger.debug "Index method called"
+    puts "Index method called"
+    query = params[:query]
+    puts "query: #{query}"
+    if query
+      list = list_to_search(query)
+      
+      @sell = list.uniq
+    else
+      @sell = []
+    end
+  end
+  
+  def list_to_search(query="")
+    filter = query
+    request_list = request_api_filters(filter)
+    list = []
+    data_list = request_list['data']
+    data_list.each do |element|
+      puts "element: #{element}"
+      id = element['id']
+      name = element['name']
+     
+      list << [id, name]
+    end
+    list
+  end
 end
